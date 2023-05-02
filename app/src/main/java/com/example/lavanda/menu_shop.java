28ce1e34be9d0d018a697c2b4d33c05a62f65961
SokSashaa <sashasokolov250603@mail.ru>
2023-05-03 01:50:16 +0300

    fixCustomAdapter

--- app/src/main/java/com/example/lavanda/menu_shop.java
+++ app/src/main/java/com/example/lavanda/menu_shop.java
@@ -1,7 +1,13 @@
 package com.example.lavanda;
 
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,34 +18,81 @@ import java.util.ArrayList;
 
 public class menu_shop extends AppCompatActivity {
 
-    ArrayList<PrAdapter> menu = new ArrayList<PrAdapter>();
+    ArrayList<CustomAdapter[]> menu1 = new ArrayList<CustomAdapter[]>();
     RecyclerView recyclerView;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_shop);
 
+        textView = (TextView) findViewById(R.id.textView8);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WindowManager.LayoutParams windowManagerParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                        WindowManager.LayoutParams.FLAG_DIM_BEHIND, PixelFormat.TRANSLUCENT);
+
+                WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View myView = inflater.inflate(R.layout.product, null);
+
+                wm.addView(myView, windowManagerParams);
+            }
+        });
 
 
-        String[] names,price,img;
-        names = new String[] {"Букет1","Букет2","Букет3"};
-        price = new String[] {"1200 р.1","1200 р.2","1200 р.3"};
-        img = new String[] {"buket","buket1","buket2"};
-        menu.add(new PrAdapter(img,names,price));
-        menu.add(new PrAdapter(img,names,price));
-        menu.add(new PrAdapter(img,names,price));
-        menu.add(new PrAdapter(img,names,price));
+        CustomAdapter customAdapter = new CustomAdapter("buket", "Букет1", "1201");
+        CustomAdapter customAdapter1 = new CustomAdapter("buket1", "Букет2", "1202");
+        CustomAdapter customAdapter2 = new CustomAdapter("buket2", "Букет3", "1203");
+        CustomAdapter[] customAdapters = new CustomAdapter[]{customAdapter, customAdapter1, customAdapter2};
+        menu1.add(customAdapters);
+        CustomAdapter customAdapter3 = new CustomAdapter("buket", "Букет4", "1204");
+        CustomAdapter customAdapter4 = new CustomAdapter("buket1", "Букет5", "1205");
+        CustomAdapter customAdapter5 = new CustomAdapter("buket2", "Букет6", "1206");
+        CustomAdapter[] customAdapters1 = new CustomAdapter[]{customAdapter3, customAdapter4, customAdapter5};
+        menu1.add(customAdapters1);
 
-         recyclerView = (RecyclerView)findViewById(R.id.rec);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rec);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // StateAdapter stateAdapter = new StateAdapter(this,menu);
-          PrAdapterHolder prAdapterHolder = new PrAdapterHolder(this,menu);
-        recyclerView.setAdapter(prAdapterHolder);
-
+        CustomAdapterHolder customAdapterHolder = new CustomAdapterHolder(this, menu1);
+        recyclerView.setAdapter(customAdapterHolder);
 
 
     }
 
+    public void Send(View v) {
+        switch (v.getId()) {
+            case R.id.image1:
+                int position = Integer.parseInt(v.getTag().toString());
+                Intent intent = new Intent(menu_shop.this, product_item.class);
+                intent.putExtra("name", menu1.get(position)[0].getNames());
+                intent.putExtra("price", menu1.get(position)[0].getPrice());
+                intent.putExtra("img", menu1.get(position)[0].getImageViews());
+                startActivity(intent);
+                break;
+            case R.id.image2:
+                int position1 = Integer.parseInt(v.getTag().toString());
+                Intent intent1 = new Intent(menu_shop.this, product_item.class);
+                intent1.putExtra("name", menu1.get(position1)[1].getNames());
+                intent1.putExtra("price", menu1.get(position1)[1].getPrice());
+                intent1.putExtra("img", menu1.get(position1)[1].getImageViews());
+                startActivity(intent1);
+                break;
+            case R.id.image3:
+                int position2 = Integer.parseInt(v.getTag().toString());
+                Intent intent2 = new Intent(menu_shop.this, product_item.class);
+                intent2.putExtra("name", menu1.get(position2)[2].getNames());
+                intent2.putExtra("price", menu1.get(position2)[2].getPrice());
+                intent2.putExtra("img", menu1.get(position2)[2].getImageViews());
+                startActivity(intent2);
+                break;
 
+        }
+    }
 }
+
--- app/src/main/java/com/example/lavanda/product_item.java
+++ app/src/main/java/com/example/lavanda/product_item.java
@@ -1,13 +1,54 @@
 package com.example.lavanda;
 
+import android.graphics.ImageDecoder;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 public class product_item extends AppCompatActivity {
+
+    ImageButton exit;
+    TextView name;
+    TextView price;
+    AppCompatImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.product);
+        Window window = getWindow();
+        window.setLayout(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        exit = (ImageButton) findViewById(R.id.exit_button);
+        name = (TextView)findViewById(R.id.name_product);
+        price = (TextView)findViewById(R.id.price_product);
+        img = (AppCompatImageView) findViewById(R.id.img_product);
+
+        String n = getIntent().getStringExtra("name");
+        String p = getIntent().getStringExtra("price");
+        String i = getIntent().getStringExtra("img");
+
+        name.setText(n);
+        price.setText(p);
+        int imgIndex = getResources().getIdentifier(i,"drawable",getPackageName());
+        img.setImageResource(imgIndex);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+
 }
